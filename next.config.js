9731472e6b683ed57ec9973632f4a8a0988f769f
MWS <meng418312436@gmail.com>
2024-08-21 23:26:22 +0800

    analytics

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com www.googletagmanager.com analytics.google.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com www.googletagmanager.com analytics.google.com app.pageview.app google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data: statichunt.com;
   media-src 'self' *.s3.amazonaws.com;
   connect-src * statichunt.com;
   font-src 'self';
   frame-src giscus.app https://www.youtube.com/ https://www.youtube-nocookie.com/
-`;
+`
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
@@ -47,7 +47,7 @@ const securityHeaders = [
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
   },
-];
+]
 
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
